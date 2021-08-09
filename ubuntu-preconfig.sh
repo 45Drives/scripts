ea2fea2f782b52fd3d65308b9b1dd47448915961
Dawson Della Valle <dawson@dellavalle.ca>
2021-08-09 11:45:08 -0300

    Change user check to be more reliable and make consistent spacing.

--- ubuntu-preconfig.sh
+++ ubuntu-preconfig.sh
@@ -14,7 +14,9 @@ if [ "$interpreter" != "bash" ]; then
 	exit 1
 fi
 
-if [[ "$EUID" != 0 ]]; then 
+euid=$(id -u)
+
+if [[ "$euid" != 0 ]]; then 
 	echo "Please run as root or with sudo."
 	exit 1
 fi
@@ -42,9 +44,10 @@ remove cloud-init and snapd, and add the Houston Management UI.
     This script should *not* be run in an SSH session, as the network will be
 modified and you may be disconnected. Run this script from the console or IPMI
 remote console.
-
 EOF
+
 	read -p "Are you sure you want to continue? [y/N]: " response
+
 	case $response in
 		[yY]|[yY][eE][sS])
 			echo
@@ -54,6 +57,7 @@ EOF
 			exit 0
 			;;
 	esac
+
 	return 0
 }
 
@@ -61,39 +65,55 @@ enable_root_user() {
 	local ROOTPASSWD=""
 	local ROOTPASSWD2=""
 	local res
-	echo "ENABLING ROOT LOGIN"
-	while true; do
+	
+    echo "ENABLING ROOT LOGIN"
+	
+    while true; do
 		read -sp "Enter password for root user: " ROOTPASSWD
-		echo
-		if [[ "$ROOTPASSWD" == "" ]]; then
+		
+        echo
+		
+        if [[ "$ROOTPASSWD" == "" ]]; then
 			echo "Root password cannot be empty! Try again."
 			continue
 		fi
-		read -sp "Confirm password for root user: " ROOTPASSWD2
-		echo
-		if [[ "$ROOTPASSWD" != "$ROOTPASSWD2" ]]; then
+		
+        read -sp "Confirm password for root user: " ROOTPASSWD2
+		
+        echo
+		
+        if [[ "$ROOTPASSWD" != "$ROOTPASSWD2" ]]; then
 			echo "Passwords do not match! Try again."
 			continue
 		fi
-		echo "root:$ROOTPASSWD" | chpasswd
-		res=$?
-		if [[ $res != 0 ]]; then
+		
+        echo "root:$ROOTPASSWD" | chpasswd
+		
+        res=$?
+		
+        if [[ $res != 0 ]]; then
 			echo "Setting password failed! Exit code: $res"
 			continue
 		fi
+
 		echo "Successfully set root password."
-		break
+		
+        break
 	done
-	return 0
+	
+    return 0
 }
 
 enable_root_ssh() {
 	local res
-	echo "ENABLING ROOT LOGIN VIA SSH"
-	cat > /etc/ssh/sshd_config.d/45drives.conf <<EOF
+	
+    echo "ENABLING ROOT LOGIN VIA SSH"
+	
+    cat > /etc/ssh/sshd_config.d/45drives.conf <<EOF
 PermitRootLogin yes
 PasswordAuthentication yes
 EOF
+
 	# Restart sshd
 	systemctl restart sshd
 	res=$?
@@ -101,17 +121,25 @@ EOF
 		echo "Restarting sshd failed!"
 		exit $res
 	fi
+
 	# test root login
 	echo "Adding localhost's ECDSA Key Fingerprint to $HOME/.ssh/known_hosts"
+
 	ssh-keyscan -H localhost >> $HOME/.ssh/known_hosts
+
 	echo "Enter password to test root login to localhost:"
+
 	ssh root@localhost exit
+
 	res=$?
+
 	if [[ $res != 0 ]]; then
 		echo "Root ssh login failed!"
 		exit $res
 	fi
+
 	echo "Successfully enabled ssh login."
+
 	return 0
 }
 
@@ -120,8 +148,8 @@ update_system() {
 	## Update system
 	# Install 45drives repository
 	echo "UPDATING SYSTEM"
-	echo "Downloading 45Drives Repo Setup Script"
 
+	echo "Downloading 45Drives Repo Setup Script"
 	curl -sSL https://repo.45drives.com/setup -o setup-repo.sh
 
 	res=$?
@@ -131,62 +159,81 @@ update_system() {
 	fi
 
 	echo "Running 45Drives Repo Setup Script"
+
 	bash setup-repo.sh
-	res=$?
-	if [[ $res != 0 ]]; then
-		echo "Failed to complete setup script! (https://repo.45drives.com/setup)"
+	
+    res=$?
+	
+    if [[ $res != 0 ]]; then
+		echo "Failed to run the setup script! (https://repo.45drives.com/setup)"
 		exit $res
 	fi
 
 	# apt upgrade
 	echo "Upgrading packages"
-	apt upgrade -y
-	res=$?
-	if [[ $res != 0 ]]; then
+	
+    apt upgrade -y
+	
+    res=$?
+	
+    if [[ $res != 0 ]]; then
 		echo "apt upgrade failed!"
 		exit $res
 	fi
 
 	echo "Successfully updated system."
-	return 0
+	
+    return 0
 }
 
 init_network() {
 	local res
-	echo "SETTING UP NETWORK MANAGER"
+	
+    echo "SETTING UP NETWORK MANAGER"
 	# Install network packages
-	apt update
-	res=$?
-	if [[ $res != 0 ]]; then
+	
+    apt update
+	
+    res=$?
+	
+    if [[ $res != 0 ]]; then
 		echo "apt update failed!"
 		exit $res
 	fi
 	
 	apt install -y network-manager firewalld
-	res=$?
-	if [[ $res != 0 ]]; then
+	
+    res=$?
+	
+    if [[ $res != 0 ]]; then
 		echo "Installing network manager and/or firewalld failed!"
 		exit $res
 	fi
 	
 	systemctl enable --now network-manager
-	res=$?
-	if [[ $res != 0 ]]; then
+	
+    res=$?
+	
+    if [[ $res != 0 ]]; then
 		echo "Enabling network manager failed!"
 		exit $res
 	fi
 	
 	# Disable ufw and enable firewalld
 	systemctl enable --now firewalld
-	res=$?
-	if [[ $res != 0 ]]; then
+	
+    res=$?
+	
+    if [[ $res != 0 ]]; then
 		echo "Enabling firewalld failed!"
 		exit $res
 	fi
 	
 	ufw disable
-	res=$?
-	if [[ $res != 0 ]]; then
+	
+    res=$?
+	
+    if [[ $res != 0 ]]; then
 		echo "Disabling ufw failed!"
 		exit $res
 	fi
@@ -198,14 +245,18 @@ init_network() {
 
 remove_garbage() {
 	local res
-	echo "REMOVING CLOUD-INIT AND SNAPD"
-	# Disable cloud-init
-	touch /etc/cloud/cloud-init.disabled
+	
+    echo "REMOVING CLOUD-INIT AND SNAPD"
+	
+    # Disable cloud-init
+    touch /etc/cloud/cloud-init.disabled
 	
 	# Remove snapd
 	apt autoremove --purge -y snapd
-	res=$?
-	if [[ $res != 0 ]]; then
+	
+    res=$?
+	
+    if [[ $res != 0 ]]; then
 		echo "Disabling snapd failed!"
 		exit $res
 	fi
@@ -217,41 +268,55 @@ remove_garbage() {
 
 add_cockpit() {
 	local res
-	echo "INITIALIZING HOUSTON"
-	# Install cockpit and cockpit related things
-	echo "Installing dependencies"
-	apt update
-	res=$?
-	if [[ $res != 0 ]]; then
+	
+    echo "INITIALIZING HOUSTON"
+	
+    # Install cockpit and cockpit related things
+	
+    echo "Installing dependencies"
+	
+    apt update
+	
+    res=$?
+	
+    if [[ $res != 0 ]]; then
 		echo "apt update failed!"
 		exit $res
 	fi
 	
 	apt install -y cockpit cockpit-zfs-manager cockpit-benchmark cockpit-navigator cockpit-file-sharing cockpit-45drives-hardware cockpit-machines cockpit-sosreport realmd tuned udisks2-lvm2 zfs-dkms samba winbind nfs-kernel-server nfs-client 45drives-tools
-	res=$?
-	if [[ $res != 0 ]]; then
+	
+    res=$?
+	
+    if [[ $res != 0 ]]; then
 		echo "Installing Houston dependencies failed!"
 		exit $res
 	fi
 	
 	# Open firewall for cockpit
 	echo "Opening firewall for cockpit"
-	firewall-cmd --permanent --add-service=cockpit
-	res=$?
-	if [[ $res != 0 ]]; then
+	
+    firewall-cmd --permanent --add-service=cockpit
+	
+    res=$?
+	
+    if [[ $res != 0 ]]; then
 		echo "Adding cockpit to firewall failed!"
 		exit $res
 	fi
 	
 	firewall-cmd --reload
-	res=$?
-	if [[ $res != 0 ]]; then
+	
+    res=$?
+	
+    if [[ $res != 0 ]]; then
 		echo "Reloading firewall failed!"
 		exit $res
 	fi
 	
 	# Install cockpit override manifests for 45ddrives-hardware and apps
-	cat >> /usr/share/cockpit/45drives-disks/override.json <<EOF
+	
+    cat >> /usr/share/cockpit/45drives-disks/override.json <<EOF
 {
 	"menu": {
 		"45drives-disks": {
@@ -260,6 +325,7 @@ add_cockpit() {
 	}
 }
 EOF
+
 	cat >> /usr/share/cockpit/45drives-motherboard/override.json <<EOF
 {
 	"menu": {
@@ -269,6 +335,7 @@ EOF
 	}
 }
 EOF
+
 	cat >> /usr/share/cockpit/45drives-system/override.json <<EOF
 {
 	"menu": {
@@ -278,6 +345,7 @@ EOF
 	}
 }
 EOF
+
 	cat >> /usr/share/cockpit/apps/override.json <<EOF
 {
 	"tools": {
@@ -285,8 +353,11 @@ EOF
 		}
 }
 EOF
+
 	systemctl enable --now cockpit.socket
+
 	res=$?
+
 	if [[ $res != 0 ]]; then
 		echo "Enabling cockpit.socket failed!"
 		exit $res
@@ -299,27 +370,37 @@ EOF
 
 use_nm_not_systemd-networkd() {
 	local res
+
 	echo "ENABLING NETWORK MANAGER"
 	# Use Network Manager instead of systemd-networkd
+
 	cat > /etc/netplan/00-networkmanager.yaml <<EOF
 network:
   version: 2
   renderer: NetworkManager
 EOF
+
 	[[ -f /etc/netplan/00-installer-config.yaml ]] && mv /etc/netplan/00-installer-config.yaml /etc/netplan/00-installer-config.yaml.backup
+
 	netplan try
+
 	res=$?
+
 	if [[ $res != 0 ]]; then
 		echo "netplan try failed."
 		exit $res
 	fi
 	
 	ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+
 	mv /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf  /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf.backup
+
 	sed -i '/^managed/s/false/true/' /etc/NetworkManager/NetworkManager.conf
 	
 	systemctl restart network-manager
+
 	res=$?
+
 	if [[ $res != 0 ]]; then
 		echo "Reloading network-manager failed."
 		exit $res
@@ -332,8 +413,11 @@ EOF
 
 setup_done() {
 	local response=""
+
 	echo "SETUP COMPLETE"
+
 	read -p "Reboot system now? [y/N]: " response
+
 	case $response in
 		[yY]|[yY][eE][sS])
 			reboot now
@@ -342,6 +426,7 @@ setup_done() {
 			echo "Reboot soon to finish configuration."
 			;;
 	esac
+
 	return 0
 }
 
