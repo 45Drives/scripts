082a54022459241b239abaaa78b540c61e6e0a1e
markdhooper <markushooper@gmail.com>
2021-11-18 11:39:26 -0400

    updated udev script

--- udev/45drives_udev_rules.sh
+++ udev/45drives_udev_rules.sh
@@ -11,8 +11,21 @@ SCRIPT_PATH="/usr/lib/udev/vdev_id_45drives"
 RULES_URL="https://scripts.45drives.com/udev/68-vdev.rules"
 SCRIPT_URL="https://scripts.45drives.com/udev/vdev_id_45drives"
 
-curl -o "$RULES_PATH" "$RULES_URL"
-curl -o "$SCRIPT_PATH" "$SCRIPT_URL"
+set -e
 
+euid=$(id -u)
+if [ $euid -ne 0 ]; then
+	echo -e '\nYou must be root to run this utility.\n'
+    exit 1
+fi
+
+echo "Downloading udev rules"
+curl -o "$RULES_PATH" "$RULES_URL"
+echo "Downloading udev script"
+curl -o "$SCRIPT_PATH" "$SCRIPT_URL"
+echo "Making script executable"
+chmod +x "$SCRIPT_PATH"
+echo "Reloading udev rules"
 udevadm control --reload-rules
+echo "Triggering udev rules"
 udevadm trigger
