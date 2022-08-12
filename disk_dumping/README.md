9c6abc0ad6d5511aa5d18de4537963c291566250
joshuaboud <joshuaboud@hotmail.com>
2022-08-12 11:33:42 -0300

    Add dump_part_info.py

---
+++ disk_dumping/README.md
@@ -0,0 +1,108 @@
+# dump_part_info.py
+Print GPT info as JSON, and optionally store in zip archive along with images of GPT and ZFS vdev label sectors of disk.
+```
+usage: dump_part_info.py [-h] [-q] [-z] [-s] BLOCK_DEVICE [OUTPUT[.zip]]
+
+Dump block device GPT and ZFS label info
+
+positional arguments:
+  BLOCK_DEVICE     Path to block device to dump
+  OUTPUT[.zip]     Path to store outputs as zip archive [optional]
+
+options:
+  -h, --help       show this help message and exit
+  -q, --quiet      Silence output, for when you only want the zip
+  -z, --zfs        Try to rip ZFS labels into archive
+  -s, --secondary  Use secondary GPT table while trying to rip ZFS images
+```
+
+# Extracting Partition Info from ZFS Physcial vDev
+Costumer has missing partitions on four drives in a raidz2 pool. We need to see if partition data is still there but only the GPT table is missing.
+## GUID Partition Table (GPT) Info
+### GPT Disk Format
+|     LBA |     offset | offset (hex) | relative offset | relative offset (hex) | description                    |
+| ------: | ---------: | -----------: | --------------: | --------------------: | ------------------------------ |
+|       0 |          0 |            0 |                 |                       | MBR                            |
+|       1 |        512 |          200 |                 |                       | Primary GPT Header             |
+|   **2** |   **1024** |      **400** |          **+0** |              **+000** | **Entry 1 (ZFS Data)**         |
+|         |       1152 |          480 |            +128 |                  +080 | Entry 2                        |
+|         |       1280 |          500 |            +256 |                  +100 | Entry 3                        |
+|         |       1408 |          580 |            +384 |                  +180 | Entry 4                        |
+|       3 |       1536 |          600 |            +512 |                  +200 | Entry 5                        |
+|         |       1664 |          680 |            +640 |                  +280 | Entry 6                        |
+|         |       1792 |          700 |            +768 |                  +300 | Entry 7                        |
+|         |       1920 |          780 |            +896 |                  +380 | Entry 8                        |
+|   **4** |   **2048** |      **800** |       **+1024** |              **+400** | **Entry 9 (Solaris Reserved)** |
+|         |            |              |                 |                       | Entries 10..128 ...            |
+|      34 |       2176 |          880 |                 |                       | Data Partition 1               |
+|         |            |              |                 |                       | Remaining Partitions ...       |
+| **-33** | **-16896** |    **-4200** |          **+0** |              **+000** | **Entry 1 (ZFS Data)**         |
+|         |     -16768 |        -4180 |            +128 |                  +080 | Entry 2                        |
+|         |     -16640 |        -4100 |            +256 |                  +100 | Entry 3                        |
+|     -32 |     -16512 |        -4080 |            +384 |                  +180 | Entry 4                        |
+|         |     -16384 |        -4000 |            +512 |                  +200 | Entry 5                        |
+|         |     -16256 |        -3F80 |            +640 |                  +280 | Entry 6                        |
+|         |     -16128 |        -3F00 |            +768 |                  +300 | Entry 7                        |
+|         |     -16000 |        -3E80 |            +896 |                  +380 | Entry 8                        |
+| **-31** | **-15872** |    **-3E00** |       **+1024** |              **+400** | **Entry 9 (Solaris Reserved)** |
+|         |            |              |                 |                       | Entries 10..128                |
+|      -1 |       -512 |         -200 |                 |                       | Secondary GPT Header           |
+
+#### GPT Header Format (LBA 1)
+| Offset    | Length   | Contents                                                                                                               |
+| --------- | -------- | ---------------------------------------------------------------------------------------------------------------------- |
+| 0  (0x00) | 8 bytes  | Signature ("EFI PART", 45h 46h 49h 20h 50h 41h 52h 54h)                                                                |
+| 8  (0x08) | 4 bytes  | Revision 1.0 (00h 00h 01h 00h) for UEFI 2.8                                                                            |
+| 12 (0x0C) | 4 bytes  | Header size in little endian (in bytes, usually 5Ch 00h 00h 00h or 92 bytes)                                           |
+| 16 (0x10) | 4 bytes  | CRC32 of header (offset +0 to +0x5b) in little endian, with this field zeroed during calculation                       |
+| 20 (0x14) | 4 bytes  | Reserved; must be zero                                                                                                 |
+| 24 (0x18) | 8 bytes  | Current LBA (location of this header copy)                                                                             |
+| 32 (0x20) | 8 bytes  | Backup LBA (location of the other header copy)                                                                         |
+| 40 (0x28) | 8 bytes  | First usable LBA for partitions (primary partition table last LBA + 1)                                                 |
+| 48 (0x30) | 8 bytes  | Last usable LBA (secondary partition table first LBA − 1)                                                              |
+| 56 (0x38) | 16 bytes | Disk GUID in mixed endian                                                                                              |
+| 72 (0x48) | 8 bytes  | Starting LBA of array of partition entries (always 2 in primary copy)                                                  |
+| 80 (0x50) | 4 bytes  | Number of partition entries in array                                                                                   |
+| 84 (0x54) | 4 bytes  | Size of a single partition entry (usually 80h or 128)                                                                  |
+| 88 (0x58) | 4 bytes  | CRC32 of partition entries array in little endian                                                                      |
+| 92 (0x5C) | *        | Reserved; zeros for rest of block (420 bytes for a sector size of 512 bytes; but can be more with larger sector sizes) |
+
+#### GPT Entry Format (LBA 2..33)
+| Offset    | Length   | Contents                                |
+| --------- | -------- | --------------------------------------- |
+| 0  (0x00) | 16 bytes | Partition type GUID (mixed endian)      |
+| 16 (0x10) | 16 bytes | Unique partition GUID (mixed endian)    |
+| 32 (0x20) | 8 bytes  | First LBA index (little endian)         |
+| 40 (0x28) | 8 bytes  | Last LBA index (inclusive, usually odd) |
+| 48 (0x30) | 8 bytes  | Attribute flags                         |
+| 56 (0x38) | 72 bytes | Partition name (36 UTF-16LE code units) |
+
+Offset / 0x200 = Offset / 512 = LBA index
+
+GPT header start: 0x200 (LBA1)
+GPT header size: 0x200
+GPT table start: 0x400
+GPT table entry size: 0x80
+
+## Commands
+### Dump LBA Sector(s)
+```bash
+dd if=/dev/sdX bs=512 skip=LBA_INDEX count=LBA_COUNT
+```
+#### Examples
+- dump MBR + primary GPT to file:
+	```bash
+	dd if=/dev/sda bs=512 skip=0 count=34 of=sda_MBR_GPT1.img
+	```
+- dump only primary GPT header of /dev/sda to file:
+	```bash
+	dd if=/dev/sda bs=512 skip=1 count=1 of=sda_GPT1_header.img
+	```
+- dump only primary GPT entries of /dev/sda to file:
+	```bash
+	dd if=/dev/sda bs=512 skip=2 count=32 of=sda_GPT1_entries.img
+	```
+- e.g. view table entry 1 as canonical hex:
+	```bash
+	dd if=/dev/sda bs=512 skip=2 count=1 | hexdump -Cv | less
+	```
---
+++ disk_dumping/dump_part_info.py
@@ -0,0 +1,327 @@
+#!/usr/bin/env python3
+
+"""
+dump_part_info.py
+Copyright (C) 2022  Josh Boudreau <jboudreau@45drives.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import argparse
+import sys
+import os
+import zlib
+import struct
+import json
+import zipfile
+from pathlib import Path
+from collections import namedtuple
+
+LBA = 512
+
+
+def bytearray2hex(raw: bytearray, join_with: str = '', fmt: str = '02X') -> str:
+    return join_with.join(format(byte, fmt) for byte in raw)
+
+
+def decode_uint8(raw: bytearray, endianness: str = '<') -> int:
+    return struct.unpack(endianness + 'B', raw)[0]
+
+
+def decode_uint16(raw: bytearray, endianness: str = '<') -> int:
+    return struct.unpack(endianness + 'H', raw)[0]
+
+
+def decode_uint32(raw: bytearray, endianness: str = '<') -> int:
+    return struct.unpack(endianness + 'L', raw)[0]
+
+
+def decode_uint64(raw: bytearray, endianness: str = '<') -> int:
+    return struct.unpack(endianness + 'Q', raw)[0]
+
+
+def decode_guid(raw: bytearray) -> str:
+    little_endians = "{:0>8X}-{:0>4X}-{:0>4X}".format(
+        *struct.unpack('<LHH', raw[:8]))
+    byte_array1 = bytearray2hex(raw[8:10])
+    byte_array2 = bytearray2hex(raw[10:])
+    return f'{little_endians}-{byte_array1}-{byte_array2}'
+
+
+def partitioned(raw: bytearray, size: int):
+    return [raw[i:i + size] for i in range(0, len(raw), size)]
+
+
+def check_chksum(section: bytearray, chksum: int):
+    result = zlib.crc32(section)
+    return {
+        'valid': chksum == result,
+        'calculated': result,
+        'should_be': chksum,
+    }
+
+
+def check_header_chksum(header: bytearray):
+    crc_offset = 0x10
+    crc_area_len = 0x5C
+    section = header[0:crc_area_len]
+    chksum = decode_uint32(section[crc_offset:crc_offset + 4])
+    section[crc_offset:crc_offset + 4] = [0] * 4  # must be zeroed
+    return check_chksum(section, chksum)
+
+
+def extract_gpt_header_info(header: bytearray):
+    GPTHeaderStruct = namedtuple(
+        'GPTHeaderStruct',
+        'signature '
+        'revision '
+        'header_size '
+        'header_crc '
+        'res1 '
+        'current_lba '  # location of this header
+        'backup_lba '  # location of backup copy
+        'first_partition_lba '
+        'last_partition_lba '
+        'disk_guid_bytearray '
+        'partition_entries_start '
+        'partition_entries_count '
+        'partition_entry_size '
+        'partition_entries_crc '
+        'res2'
+    )
+    raw_header = GPTHeaderStruct._make(
+        struct.unpack(
+            '<'  # use little-endian
+            '8s'  # signature
+            '4s'  # revision
+            'L'  # header_size
+            'L'  # header_crc
+            '4s'  # reserved
+            'Q'  # current_lba
+            'Q'  # backup_lba
+            'Q'  # first_partition_lba
+            'Q'  # last_partition_lba
+            '16s'  # disk_guid
+            'Q'  # partition_entries_start
+            'L'  # partition_entries_count
+            'L'  # partition_entry_size
+            'L'  # partition_entries_crc
+            '420s',  # reserved
+            header
+        )
+    )
+    return {
+        'signature': raw_header.signature.decode('ascii'),
+        'checksum': check_header_chksum(header),
+        'lba_indices': {
+            'this_gpt_header': raw_header.current_lba,
+            'other_gpt_header': raw_header.backup_lba,
+            'first_partition': raw_header.first_partition_lba,
+            'last_partition': raw_header.last_partition_lba,
+            'entry_table_start': raw_header.partition_entries_start,
+        },
+        'entry_table_info': {
+            'lba_index': raw_header.partition_entries_start,
+            'entry_count': raw_header.partition_entries_count,
+            'entry_size': raw_header.partition_entry_size,
+            'crc': raw_header.partition_entries_crc,
+        }
+    }
+
+
+def extract_gpt_entry_attributes(flags: int):
+    return {
+        'raw': flags,
+        'platform_required': bool(flags & (1 << 0)),
+        'efi_should_ignore': bool(flags & (1 << 1)),
+        'legacy_bios_bootable': bool(flags & (1 << 2)),
+    }
+
+
+def extract_gpt_entry(entry_raw: bytearray):
+    GPTEntryStruct = namedtuple(
+        'GPTEntryStruct',
+        'type_guid '
+        'unique_guid '
+        'first_lba '
+        'last_lba '
+        'attribute_flags '
+        'name'
+    )
+    raw_fields = GPTEntryStruct._make(struct.unpack(
+        '<'  # little-endian
+        '16s'
+        '16s'
+        'Q'
+        'Q'
+        'Q'
+        '72s',
+        entry_raw
+    ))
+    return {
+        'type_guid': decode_guid(raw_fields.type_guid),
+        'unique_guid': decode_guid(raw_fields.unique_guid),
+        'first_lba': raw_fields.first_lba,
+        'last_lba': raw_fields.last_lba,
+        'attributes': extract_gpt_entry_attributes(raw_fields.attribute_flags),
+        'name': raw_fields.name.decode('utf-16').replace('\u0000', '')
+    }
+
+
+def extract_gpt_entries(entries_raw: bytearray, entry_info: dict):
+    entries = {
+        "checksum": check_chksum(entries_raw, entry_info['crc'])
+    }
+    entry_size = 0x80
+    for (i, entry_raw) in enumerate(partitioned(entries_raw, entry_size)):
+        if not all(byte == 0 for byte in entry_raw):
+            entries[f'p{i + 1}'] = extract_gpt_entry(entry_raw)
+    return entries
+
+
+def extract_gpt_info(header_raw: bytearray, entries_raw: bytearray):
+    header = extract_gpt_header_info(header_raw)
+    return {
+        'header': header,
+        'entries': extract_gpt_entries(
+            entries_raw, header["entry_table_info"])
+    }
+
+
+def get_gpt(dev_path: str):
+    primary_gpt_raw = bytearray(33 * LBA)
+    secondary_gpt_raw = bytearray(33 * LBA)
+    with open(dev_path, 'rb') as dev_file:
+        dev_file.seek(1 * LBA)
+        dev_file.readinto(primary_gpt_raw)
+        primary_gpt = extract_gpt_info(
+            primary_gpt_raw[0:LBA], primary_gpt_raw[LBA:])
+        assumed_secondary_gpt_entries_offset = dev_file.seek(
+            -1 * len(secondary_gpt_raw), os.SEEK_END)
+        dev_file.readinto(secondary_gpt_raw)
+        secondary_gpt = extract_gpt_info(
+            secondary_gpt_raw[-LBA:], secondary_gpt_raw[:-LBA])
+        secondary_gpt["header"]["lba_indices"]["meta"] = {
+            'secondary_gpt_entries_after_last_partition_lba': assumed_secondary_gpt_entries_offset == (
+                primary_gpt["header"]["lba_indices"]["last_partition"] + 1) * LBA
+        }
+        return {
+            'primary': primary_gpt,
+            'secondary': secondary_gpt,
+        }
+
+
+# def get_zfs_nvlist(dev_path: str):
+
+def validate_args(args):
+    valid = True
+    if not Path(args.input_device).exists():
+        print(
+            f'Error: {args.input_device} does not exist!', file=sys.stderr)
+        valid = False
+    elif not Path(args.input_device).is_block_device():
+        print(
+            f'Warning: {args.input_device} is not a block device!', file=sys.stderr)
+        if not Path(args.input_device).is_file():
+            print(
+                f'Error: {args.input_device} is not a block device or regular file!', file=sys.stderr)
+            valid = False
+    return valid
+
+
+def rip_images(dev_path: str, archive: zipfile.ZipFile):
+    with open(dev_path, 'rb') as dev_file:
+        archive.writestr('LBA0_LBA33_MBR_GPT1.img', dev_file.read(34 * LBA))
+        dev_file.seek(-33 * LBA, os.SEEK_END)
+        archive.writestr('LBA-33_LBA-0_GPT2.img', dev_file.read())
+
+
+def rip_zfs(dev_path: str, archive: zipfile.ZipFile, gpt_info: dict, use_secondary: bool):
+    gpt = 'secondary' if use_secondary else 'primary'
+    ZFS_LABEL_LEN = 256 * 1024
+    with open(dev_path, 'rb') as dev_file:
+        # part1 (ZFS)
+        # whole labels
+        zfs_data_start = gpt_info[gpt]['entries']['p1']['first_lba'] * LBA
+        zfs_data_end = (gpt_info[gpt]['entries']
+                        ['p1']['last_lba'] + 1) * LBA  # exclusive
+        dev_file.seek(zfs_data_start)
+        archive.writestr('ZFS_vdev_label_0.img', dev_file.read(ZFS_LABEL_LEN))
+        archive.writestr('ZFS_vdev_label_1.img', dev_file.read(ZFS_LABEL_LEN))
+        dev_file.seek(zfs_data_end - (2 * ZFS_LABEL_LEN))
+        dev_file.seek(-2 * ZFS_LABEL_LEN, os.SEEK_CUR)
+        archive.writestr('ZFS_vdev_label_2.img', dev_file.read(ZFS_LABEL_LEN))
+        archive.writestr('ZFS_vdev_label_3.img', dev_file.read(ZFS_LABEL_LEN))
+        # nv_pairs
+        NVLIST_OFFSET = 16 * 1024
+        NVLIST_LENGTH = (128 - 16) * 1024
+        dev_file.seek(zfs_data_start + NVLIST_OFFSET)
+        archive.writestr('ZFS_vdev_label_0_nvlist.img', dev_file.read(NVLIST_LENGTH))
+        dev_file.seek(ZFS_LABEL_LEN - NVLIST_LENGTH, os.SEEK_CUR)
+        archive.writestr('ZFS_vdev_label_1_nvlist.img', dev_file.read(NVLIST_LENGTH))
+        dev_file.seek(zfs_data_end - (2 * ZFS_LABEL_LEN) + NVLIST_OFFSET)
+        archive.writestr('ZFS_vdev_label_2_nvlist.img', dev_file.read(NVLIST_LENGTH))
+        dev_file.seek(ZFS_LABEL_LEN - NVLIST_LENGTH, os.SEEK_CUR)
+        archive.writestr('ZFS_vdev_label_3_nvlist.img', dev_file.read(NVLIST_LENGTH))
+        # part9 (Solaris Reserved 1, 8M usually)
+        PART9_START = gpt_info[gpt]['entries']['p9']['first_lba'] * LBA
+        PART9_END = (gpt_info[gpt]['entries']
+                        ['p9']['last_lba'] + 1) * LBA  # exclusive
+        PART9_LEN = PART9_END - PART9_START
+        dev_file.seek(PART9_START)
+        archive.writestr('ZFS_solaris_reserved.img', dev_file.read(PART9_LEN))
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Dump block device GPT and ZFS label info')
+    parser.add_argument('input_device', metavar='BLOCK_DEVICE',
+                        type=str, help='Path to block device to dump')
+    parser.add_argument('out_path', metavar='OUTPUT[.zip]',
+                        help='Path to store outputs as zip archive [optional]', nargs='?', default=None)
+    parser.add_argument('-q', '--quiet', action='store_true',
+                        help='Silence output, for when you only want the zip')
+    parser.add_argument('-z', '--zfs', action='store_true',
+                        help='Try to rip ZFS labels into archive')
+    parser.add_argument('-s', '--secondary', action='store_true', help='Use secondary GPT table while trying to rip ZFS images')
+
+    args = parser.parse_args()
+
+    if not validate_args(args):
+        sys.exit(1)
+
+    gpt_info_json = ""
+
+    gpt_info = get_gpt(args.input_device)
+
+    gpt_info_json = json.dumps(gpt_info, indent=2)
+
+    if not args.quiet:
+        print(gpt_info_json)
+
+    if args.out_path:
+        out_path = Path(args.out_path)
+        if (out_path.suffix != '.zip'):
+            out_path = out_path.with_suffix(out_path.suffix + '.zip')
+        if (out_path.exists()):
+            print(f'Error: {str(out_path)} exists!')
+            sys.exit(1)
+        with zipfile.ZipFile(out_path, mode='w', allowZip64=True, compression=zipfile.ZIP_DEFLATED) as archive:
+            archive.writestr('gpt_info.json', gpt_info_json)
+            rip_images(args.input_device, archive)
+            if args.zfs:
+                rip_zfs(args.input_device, archive, gpt_info, args.secondary)
+
+
+if __name__ == "__main__":
+    main()
