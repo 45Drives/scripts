a186bf768b24129a6e810bfc7e36e61d3c7d2c12
joshuaboud <joshuaboud@hotmail.com>
2022-11-02 14:48:34 -0300

    add more allowed chars and fix chaning walk lists

--- fix_mangled_paths.py
+++ fix_mangled_paths.py
@@ -8,34 +8,44 @@ import argparse
 import re
 
 
-def get_unique_name(parent_path, name, taken_paths):
+def get_unique_name(root, name, taken_paths):
     i = 0
-    new_path = os.path.join(parent_path, name)
     extension_dot_ind = name.find('.')
+    new_name = name
+    new_path = os.path.join(root, new_name)
     while new_path in taken_paths or os.path.exists(new_path):
         i += 1
         new_name = name[0:extension_dot_ind] + f"({i})" + name[extension_dot_ind:] if extension_dot_ind != -1 else name + f"({i})"
-        new_path = os.path.join(parent_path, new_name)
+        new_path = os.path.join(root, new_name)
     taken_paths.append(new_path)
-    return new_path
+    return new_name
 
 
 def rename_mangled(paths, dry_run):
     taken_paths = []
+    def rename_entries(root, entries, rootfd):
+        changes = []
+        for src in entries:
+            dst = re.sub('[^A-Za-z0-9 _\-,.\(\)\'+!@]', '_', src, flags=re.MULTILINE)
+            if dst != src:
+                dst = get_unique_name(root, dst, taken_paths)
+                print(os.path.join(root, src), ' -> ', os.path.join(root, dst))
+                if dry_run:
+                    continue
+                try:
+                    os.rename(src, dst, src_dir_fd=rootfd, dst_dir_fd=rootfd)
+                    changes.append((src, dst))
+                except OSError as e:
+                    print('failed to rename file:', e)
+        for src, dst in changes:
+            entries.remove(src)
+            entries.append(dst)
     for path in paths:
-        for root, dirs, files in os.walk(path, topdown=False):
-            for name in [*dirs, *files]:
-                new_name = re.sub('[^A-Za-z0-9 _\-,.\(\)]', '_', name, flags=re.MULTILINE)
-                if new_name != name:
-                    src = os.path.join(root, name)
-                    dst = get_unique_name(root, new_name, taken_paths)
-                    print(src, ' -> ', dst)
-                    if dry_run:
-                        continue
-                    try:
-                        os.rename(src, dst)
-                    except OSError as e:
-                        print('failed to rename file:', e)
+        for root, dirs, files, rootfd in os.fwalk(path, topdown=True):
+            if '.zfs' in dirs:
+                dirs.remove('.zfs')
+            rename_entries(root, dirs, rootfd)
+            rename_entries(root, files, rootfd)
 
 
 def main():
