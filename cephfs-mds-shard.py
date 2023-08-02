dfeff632d48556ffafdfd91314a1f28c1d132a49
mitcHELLspawn <50410353+mitcHELLspawn@users.noreply.github.com>
2023-08-02 16:02:55 -0300

    quality of life fixes/features
    added help menu and ability to run in dry run mode.
    
    Script will no longer attempt to pin a directory if it has already been pinned previously.

--- cephfs-mds-shard.py
+++ cephfs-mds-shard.py
@@ -1,34 +1,68 @@
 #!/usr/bin/env python3
 # Mitch Hall
 # 45Drives
-# Version 1.0 - July 6/23
+# Version 1.1 - July 31/23
 import os
 import json
 import time
+import argparse
 import subprocess
-from hashlib import sha1
+import re
 
-# find out which dir to shard
-topdir = input('Which top level directory do you want to shard? (This will loop through all sub dirs and pin each to a different MDS) ')
+def main():
+    parser = argparse.ArgumentParser(description='Shard directories and pin each to a different MDS.')
+    parser.add_argument('-d', '--dir', help='The top-level directory to shard.')
+    parser.add_argument('-D', '--dry-run', action='store_true', help='Run the script in dry run mode. No actions will be performed.')
+    args = parser.parse_args()
 
-# get max_mds info
-fs_dump = subprocess.check_output("ceph fs dump --format json 2>/dev/null", shell=True)
-max_mds = json.loads(fs_dump)["filesystems"][0]["mdsmap"]["max_mds"]
+    if args.dir:
+        # wait for health ok
+        while not "HEALTH_OK" in subprocess.check_output("ceph health 2>/dev/null", shell=True).decode():
+            print("Waiting 10s for HEALTH_OK...")
+            time.sleep(10)
 
-# loop through the dirs and pin to available MDS
-dirs = sorted([d for d in os.listdir(topdir) if os.path.isdir(os.path.join(topdir, d))])
-for i, dir in enumerate(dirs):
-    full_dir_path = os.path.join(topdir, dir)
+        # get max_mds info
+        fs_dump = subprocess.check_output("ceph fs dump --format json 2>/dev/null", shell=True)
+        max_mds = json.loads(fs_dump)["filesystems"][0]["mdsmap"]["max_mds"]
 
-    # wait for health ok
-    while not "HEALTH_OK" in subprocess.check_output("ceph health 2>/dev/null", shell=True).decode():
-        print("Waiting 10s for HEALTH_OK...")
-        time.sleep(10)
+        # loop through the dirs and pin to available MDS
+        dirs = sorted([d for d in os.listdir(args.dir) if os.path.isdir(os.path.join(args.dir, d))])
+        
+        next_mds = 0
 
-    # calculate the MDS rank
-    mds_rank = i % max_mds
+        for dir in dirs:
+            full_dir_path = os.path.join(args.dir, dir)
 
-    # pin the dir
-    print(f"Pinning {dir} to {mds_rank}")
-    subprocess.run(f'setfattr -n ceph.dir.pin -v {mds_rank} "{full_dir_path}"', shell=True)
-    time.sleep(1)
+            # check if MDS already pinned
+            pinned_mds = subprocess.check_output(f'getfattr -n ceph.dir.pin "{full_dir_path}" 2>/dev/null', shell=True).decode()
+
+            if "ceph.dir.pin" in pinned_mds:
+                current_mds = re.search('ceph.dir.pin="(.*)"', pinned_mds).group(1)
+                if current_mds != "-1":
+                    print(f"{dir} is already pinned by MDS {current_mds}")
+                else:
+                    if args.dry_run:
+                        # print the action to be performed
+                        print(f"Would pin {dir} to {next_mds} - Remove dry run flag to set")
+                    else:
+                        # pin the dir
+                        print(f"Pinning {dir} to MDS {next_mds}")
+                        subprocess.run(f'setfattr -n ceph.dir.pin -v {next_mds} "{full_dir_path}"', shell=True)
+                    next_mds = (next_mds + 1) % max_mds
+                    time.sleep(1)
+            else:
+                if args.dry_run:
+                    # print the action to be performed
+                    print(f"Would pin {dir} to {next_mds} - Remove dry run flag to set")
+                else:
+                    # If ceph.dir.pin is not present, pin the dir
+                    print(f"Pinning {dir} to MDS {next_mds}")
+                    subprocess.run(f'setfattr -n ceph.dir.pin -v {next_mds} "{full_dir_path}"', shell=True)
+                next_mds = (next_mds + 1) % max_mds
+                time.sleep(1)
+    else:
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
+
