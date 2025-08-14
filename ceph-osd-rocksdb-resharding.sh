9b860322b53e4b1e1954e166c1c665b1ff59946d
Bailey Allison <baileyjallison@gmail.com>
2025-08-14 16:50:42 -0300

    fix typo in script

--- ceph-osd-rocksdb-resharding.sh
+++ ceph-osd-rocksdb-resharding.sh
@@ -12,10 +12,12 @@ for i in $(ceph osd ls-tree $(hostname -s)); do
     fi
     echo "$OSD_DIR exists on $(hostname -s), running reshard"
     set -x
+    systemctl stop ceph-osd@$i
     ceph-bluestore-tool --path $OSD_DIR fsck
     ceph-bluestore-tool --path $OSD_DIR --sharding="m(3) p(3,0-12) O(3,0-13)=block_cache={type=binned_lru} L P" reshard
     ceph-bluestore-tool --path $OSD_DIR show-sharding
     ceph-bluestore-tool --path $OSD_DIR fsck
+    systemctl start ceph-osd@$i
     set +x
 done
 ceph osd unset noout
