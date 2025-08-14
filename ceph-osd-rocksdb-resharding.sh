f41b2b3ff407fc8c77d8afb1c14995fe4a512a34
Bailey Allison <baileyjallison@gmail.com>
2025-08-14 16:43:48 -0300

    add script for ceph-osd-rocksdb-resharding

---
+++ ceph-osd-rocksdb-resharding.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#rocksdb-resharding
+
+ceph osd set noout
+
+for i in $(ceph osd ls-tree $(hostname -s)); do
+    echo "Resharding OSD $i"
+    OSD_DIR="/var/lib/ceph/osd/ceph-$i";
+    if [ ! -d "$OSD_DIR"]; then
+        echo "$OSD_DIR does not exist on $(hostname -s), skipping"
+        continue
+    fi
+    echo "$OSD_DIR exists on $(hostname -s), running reshard"
+    set -x
+    ceph-bluestore-tool --path $OSD_DIR fsck
+    ceph-bluestore-tool --path $OSD_DIR --sharding="m(3) p(3,0-12) O(3,0-13)=block_cache={type=binned_lru} L P" reshard
+    ceph-bluestore-tool --path $OSD_DIR show-sharding
+    ceph-bluestore-tool --path $OSD_DIR fsck
+    set +x
+done
+ceph osd unset noout
