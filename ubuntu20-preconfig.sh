3b7e05dc2acce7e0080d446aa05563ef0eef2ca3
curtis-leblanc <cjl199524@gmail.com>
2026-01-30 15:39:29 -0400

    Removed cockpit-sosreport from Ubuntu20Preconfig script as package isn't present in focal repo and script would fail as a result.

--- ubuntu20-preconfig.sh
+++ ubuntu20-preconfig.sh
@@ -294,7 +294,7 @@ add_cockpit() {
 	fi
 	
 	apt install -y cockpit cockpit-benchmark cockpit-navigator cockpit-file-sharing cockpit-45drives-hardware cockpit-identities  \
-		cockpit-sosreport realmd tuned udisks2-lvm2 zfs-dkms samba winbind nfs-kernel-server nfs-client 45drives-tools cockpit-scheduler cockpit-zfs nano tuned 
+		realmd tuned udisks2-lvm2 zfs-dkms samba winbind nfs-kernel-server nfs-client 45drives-tools cockpit-scheduler cockpit-zfs nano tuned 
 	
     res=$?
 	
